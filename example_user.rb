5e2def012c85c80e742d5bbf3838e7f7c6badbd4
Mike Remick <mremic01@gmail.com>
2012-02-27 17:51:56 -0600

    finished first cut of the user model -end of ch6

---
+++ example_user.rb
@@ -0,0 +1,19 @@
+class User
+  attr_accessor :name, :email
+
+  def initialize(attributes = {})
+    @name  = attributes[:name]
+    @email = attributes[:email]
+  end
+
+  def formatted_email
+    "#{@name} <#{@email}>"
+  end
+  
+  def shuffle
+    @name.split.shuffle.join
+  end
+
+
+
+end
